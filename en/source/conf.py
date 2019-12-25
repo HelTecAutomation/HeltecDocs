e5626d8411e4c2b8ca92fc2368624adde5957c28
Heltec-Aaron-Lee <support@heltec.cn>
2019-12-25 18:26:54 +0800

    Change theme

--- en/source/conf.py
+++ en/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'HeltecDocs_en'
+project = 'Heltec Automation Docs'
 copyright = '2019, Heltec'
 author = 'Heltec'
 master_doc = 'index'
@@ -31,8 +31,7 @@ release = 'V0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
